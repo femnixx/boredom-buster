28f9e015d5fea36af2e692daf2dfd4f48cb77d47
femnixx <femnitality@gmail.com>
2026-06-17 13:25:00 +0700

    feat: finish style-less login

---
+++ src/components/FloatingLoginBanner.tsx
@@ -0,0 +1,16 @@
+function FloatingLoginBanner() { 
+    return (
+        <>
+        <div className="flex">
+            <div>Icon</div>
+            <div>
+                <p>NEW ACHIEVEMENT</p>
+                <p>Master of Focus</p>
+                <p>Completed 5 tasks in under 2 hours today!</p>
+            </div>
+        </div>
+        </>
+    )
+}
+
+export default FloatingLoginBanner;
